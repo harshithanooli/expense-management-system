7201dfa4a563d968b903a52b41659c8b6d2dc2ee
harshithanooli <harshithanooli2000@gmail.com>
2025-06-10 01:22:01 -0400

    all changes commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,23 +1,30 @@
 const userModel = require('../models/userModel')
+const bcrypt = require('bcrypt');
 
 //login callback
 const loginController = async (req, res) => {
     try {
         const { email, password } = req.body
-        const user = await userModel.findOne({ email, password })
+        const user = await userModel.findOne({ email });
+    
         if (!user) {
             return res.status(404).send('User Not Found')
         }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).send('Invalid Email or Password');
+        }
         res.status(200).json({
             success: true,
-            user,
+            user: { ...user._doc, password: undefined },
         });
     }
     catch (error) {
         res.status(400).json({
             success: false,
             error,
-        })
+        });
     }
 };
 
@@ -25,12 +32,23 @@ const loginController = async (req, res) => {
 const registerController = async (req, res) => {
 
     try{
-        const newUser = new userModel(req.body)
+        const existingUser = await userModel.findOne({ email : req.body.email });
+        if(existingUser) {
+            return res.status(400).json({ success:  false, message: 'User already exists' });
+        }
 
-        await newUser.save()
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const newUser = new userModel({
+            ...req.body,
+            password: hashedPassword,
+        });
+
+        
+        await newUser.save();
+
         res.status(201).json({
             success:true,
-            newUser,
+            user: { ...newUser._doc, password: undefined },
         })
     }
     catch(error){
